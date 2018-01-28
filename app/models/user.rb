2a6f88cadc1a4307aed4ba4a9d973a4f2a6f0bb0
Peppercat02 <cheny.pili@gmail.com>
2018-01-28 23:29:12 +0800

    fix presenting admin bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :trackable, :validatable
 
     validates_presence_of :name
-  mount_uploader :avatar, AvatarUploader
+  mount_uploader :avatar, PhotoUploader
 
 
   # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
